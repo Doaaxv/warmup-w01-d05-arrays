4050e1fdbd0c6578f4fe07596a0e95302c1b5527
Doaa AlHajjaji <Doaa.mh.uni@gmail.com>
2019-10-03 09:40:20 +0300

    add if null and empty condition

--- w01d05.js
+++ w01d05.js
@@ -3,10 +3,15 @@ function positiveAndNegativeOp(array) {
     let oddSum = 0
 
     for (let i = 0; i < array.length; i++) {
+        if(array.length == 0 || array == null){
         const answer = array[i] > 0 ? positiveCount += 1 : oddSum += array[i]
 
-    }
+    
     return [positiveCount, oddSum]
+        }else{
+        return []
+    }
+}
 }
 
-console.log(positiveAndNegativeOp([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+console.log(positiveAndNegativeOp([null]))
